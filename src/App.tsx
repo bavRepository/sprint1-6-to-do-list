3dc6909f19097775a904dcc1ba8dfcca58bb6687
Anton <a.barai6003@gmail.com>
2025-07-25 07:12:50 +0300

    sprint-1-add-project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { TodolistItem } from './TodolistItem.tsx'
+
+function App() {
+  return (
+    <div className='app'>
+      <TodolistItem />
+      <TodolistItem />
+      <TodolistItem />
+    </div>
+  )
+}
+
+export default App
